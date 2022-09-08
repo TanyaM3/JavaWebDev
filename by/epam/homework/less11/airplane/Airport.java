579be5ee7497dbdc8211d4e3b4d11e274a5f5ec8
TatsianaMalashankava <109862123+TanyaM3@users.noreply.github.com>
2022-09-08 19:01:49 +0300

    Add files via upload

--- by/epam/homework/less11/airplane/Airport.java
+++ by/epam/homework/less11/airplane/Airport.java
@@ -1,22 +1,23 @@
 package by.epam.homework.less11.airplane;
 
-import java.io.Serializable;
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Airport implements Serializable{
-	private static final long serialVersionUID = 1L;
+public class Airport extends Airplane {
+
+	private List<Airplane> airplanes = new ArrayList<Airplane>();
 	
-	private List<Airplane> airplanes = new ArrayList<Airplane>();;
+	public Airport(String destination, int flightNumber, String planeType, Time departureTime, String day,
+			List<Airplane> airplanes) {
+		super(destination, flightNumber, planeType, departureTime, day);
+		this.airplanes = airplanes;
+	}
 	
 	public Airport() {
-		new ArrayList<Airplane>();
+		
 	}
-	
-	public Airport(List<Airplane> airplanes) {
-		this.airplanes = airplanes;;
-	}
-	
+
 	public List<Airplane> getAirplanes() {
 		return airplanes;
 	}
@@ -27,9 +28,5 @@ public class Airport implements Serializable{
 
 	public void addPlane(Airplane plane) {
 		airplanes.add(plane);
-	}
-	
-	public void deletePlane(Airplane plane) {
-		airplanes.remove(plane);
 	}
 }
